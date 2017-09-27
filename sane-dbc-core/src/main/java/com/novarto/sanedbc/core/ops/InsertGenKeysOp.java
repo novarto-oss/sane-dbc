d5e365b4eb2077ec1af0fa5409dce02260c11c7a
Radoslav Smilyanov <rsmilyanov@gmail.com>
2017-09-27 13:40:00 +0300

    Add javadoc for BatchInsertGenKeysOps and InsertGenKeysOp (#4)
    * Add javadoc for BatchInsertGenKeysOps and InsertGenKeysOp
    
    * Add @see with link to java.sql.Statement.getGeneratedKeys()
    
    * typo
    
    * typo

--- sane-dbc-core/src/main/java/com/novarto/sanedbc/core/ops/InsertGenKeysOp.java
+++ sane-dbc-core/src/main/java/com/novarto/sanedbc/core/ops/InsertGenKeysOp.java
@@ -6,6 +6,14 @@ import fj.function.TryEffect1;
 
 import java.sql.*;
 
+/**
+ * Insert operation which returns the auto-generated key created for the inserted entry. Please consider that the used JDBC driver
+ * may not support the retrieval of auto-generated key and this operation may fail with {@link SQLFeatureNotSupportedException}
+ *
+ * @param <A> the type of the result which must extend {@link Number}
+ * @see Statement#getGeneratedKeys()
+ */
+
 public abstract class InsertGenKeysOp<A extends Number> extends DB<A>
 
 {
